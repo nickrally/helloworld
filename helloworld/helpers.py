8a238593b2b38421a951f8f394634f799fdb53a1
n <n@pop-os.localdomain>
2022-11-15 21:43:00 -0700

    commit 2 on feature-two

--- helloworld/helpers.py
+++ helloworld/helpers.py
@@ -1,8 +1,8 @@
 def readwrite(original, modified):
     with open(original, 'rb') as infile:
         with open(modified, 'wb') as outfile:
-            for line in infile:
-                outfile.write(line.upper())
+            for i, line in enumerate(infile):
+                outfile.write(i + " " +line.upper())
 
 def what_is(thing):
     print(f'What is this {thing} really?')
