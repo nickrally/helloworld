12ae64605ecc0213b9290e00069a8aa8947d29f9
n <n@pop-os.localdomain>
2022-11-15 23:57:53 -0700

    commit 4 on feature-two

--- helloworld/helloworld.py
+++ helloworld/helloworld.py
@@ -5,19 +5,21 @@ class Helloworld:
     ids_list = []
 
     @classmethod
-    def is_id_taken(cls, i):
-        if (i in cls.ids_list):
-            return True
+    def is_id_valid(cls, i):
+        is_valid = False
+        if i.is_integer() and not i in cls.ids_list :
+            is_valid = True
+        return is_valid
 
 
     def __init__(self, i):
-        if not Helloworld.is_id_taken(i):
+        if Helloworld.is_id_valid(i):
             self.id = i
             Helloworld.count += 1
             Helloworld.ids_list.append(i)
         else:
             print(f'{i} is taken, assigning timestamp...')
-            self.id = int(time.time())
-
+            self.id = int(time.time()) + 42
+        
     def __repr__(self):
         return f'Not one more hello world! {self.id}'
