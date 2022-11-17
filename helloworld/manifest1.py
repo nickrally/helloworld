2d9ad5adad50ef1281867c8f5e2fb4659395cd3b
n <n@pop-os.localdomain>
2022-11-16 20:37:26 -0700

    manifest files 1-3

--- helloworld/manifest1.py
+++ helloworld/manifest1.py
@@ -1,14 +1,14 @@
-{
-    'name': 'Foo',
-    'version': '0.1',
-    'description': """
-    This module adds functionality for
+{ 
+    'name': 'Foo', 
+    'version': '0.1', 
+    'description': """ 
+    This module adds functionality for  
          foo
-    """,
-    'author': 'Pushkin',
-    'depends': ['base'],
-    'data': ['some_view.xml'],
-    'demo': [],
-    'installable': False,
-    'auto_install': True,
+    """, 
+    'author': 'Pushkin', 
+    'depends': ['base'], 
+    'data': ['some_view.xml'], 
+    'demo': [], 
+    'installable': True, 
+    'auto_install': False, 
 }
