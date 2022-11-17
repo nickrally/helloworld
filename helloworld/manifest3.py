52858e942b8c4cf9e43ea594f4b4a587f0cf58cd
Nik Musaelian <nik@ursamajor.com>
2022-11-16 20:53:02 -0700

    manifest 1,2,3

---
+++ helloworld/manifest3.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Fookabane',
+    'version': '0.1',
+    'description': """
+    This module adds functionality for
+         fookabane
+    """,
+    'author': 'Karabas',
+    'depends': ['base', 'foo'],
+    'data': ['some_view.xml'],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+}
